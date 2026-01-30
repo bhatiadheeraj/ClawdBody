55a8d911f4d5865d1b1cfef65e44f1a62b36ff20
Prakshal Jain <prakshal@buffalo.edu>
2026-01-29 19:55:19 -0800

    Fixed build errors

--- src/app/api/setup/orgo/create-project/route.ts
+++ src/app/api/setup/orgo/create-project/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
 import { prisma } from '@/lib/prisma'
-import { OrgoClient, generateComputerName } from '@/lib/orgo'
+import { OrgoClient } from '@/lib/orgo'
 import { decrypt } from '@/lib/encryption'
 
 /**
